284ef2ccab2a3a23df4be0d2ed8ae6c8561369f2
punisher0312 <mohbdllh@gmail.com>
2021-08-13 21:37:38 +0530

    Simple improvement

--- clear.py
+++ clear.py
@@ -1,5 +1,6 @@
-#This piece of code is written for the functioning on termux only
-import os 
+#This piece of code is written to work on termux only
+import os
 def clear():
 	os.system("clear")
+return
 
