b01ee61f9aa001f725bd1e791393fa4c5dbf1391
Mohd Abdullah <63020306+mohdabdullah0312@users.noreply.github.com>
2023-04-26 06:27:12 +0530

    Update start.py

--- starting_func_call/start.py
+++ starting_func_call/start.py
@@ -1,3 +1,3 @@
 from os import system as sy
 sy("apt update && apt list --upgradeable && apt upgrade -y")
-print("You are now working on a conpletely updated platform!!!")
+print("You are now working on a completely updated platform!!!")
